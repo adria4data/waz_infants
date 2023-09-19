9309afa6761497f2ca629d7a2ef82e2b25083535
adria4data <141827371+adria4data@users.noreply.github.com>
2023-09-19 05:50:12 -0500

    Setting the working directory Importing the dataset

--- script/waz_infants_script.R
+++ script/waz_infants_script.R
@@ -13,6 +13,37 @@
 
 # 2. Data Exploration------------------------------------------------------------
 
+# 2.1. Setting Up the Environment: R Packages Installation----------------------
+
+install.packages("tidyverse")
+install.packages("moments")
+install.packages(c("lmtest", "car", "carData"))
+
+library(tidyverse)
+library (moments)
+library(lmtest)
+library(car)
+library(carData)
+
+# 2.2. Setting the Working Directory------------------------------------------
+
+# Check the working directory
+
+getwd()
+
+# For setting a new working directory use the setwd()
+# setwd("C:\\Users\\adria\\Documents\\waz_infants")
+
+# List Files in Directory To see a list of all files in the current working directory, you can use
+
+list.files()
+
+# 2.3. Import and Visualize the dataset-----------------------------------------
+
+library(readr)
+waz_final <- read_csv("data/waz_final.csv")
+View(waz_final)
+
 
 # 3. Dependent Variable (DV) and Data Processing---------------------------------
 
