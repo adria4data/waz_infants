3c06ddb806700a778a45f1ac1792919e8cddf604
adria4data <141827371+adria4data@users.noreply.github.com>
2023-09-19 05:24:48 -0500

    Research objective update

--- script/waz_infants_script.R
+++ script/waz_infants_script.R
@@ -1,11 +1,18 @@
 
-# Predictors of Weight in RomanianInfants: Weight-for-age z-scores (WAZ)
+# Predictors of Weight in Romanian Infants: Weight-for-age z-scores (WAZ)
 
 # Adrian Rus (2023)
 
+
+
 # 1. Research Hypotheses/Questions/Objectives------------------------------------
 
+# The objective of the present study was to explore the relationship between infants’ weight and various demographics 
+# (i.e., infants’ mother location, marital status, social economic status, and age; and infants being born on term, age, anemia status, rank into the family and dietary patterns).
+# Identifying the connection between such factors could help practitioners in developing public health policies and interventions in Romania.
+
 # 2. Data Exploration------------------------------------------------------------
+
 
 # 3. Dependent Variable (DV) and Data Processing---------------------------------
 
