773e374ad149572a7424f261a40509f5c396aab3
adria4data <141827371+adria4data@users.noreply.github.com>
2023-09-19 05:20:45 -0500

    Script structure update

---
+++ script/waz_infants_script.R
@@ -0,0 +1,17 @@
+
+# Predictors of Weight in RomanianInfants: Weight-for-age z-scores (WAZ)
+
+# Adrian Rus (2023)
+
+# 1. Research Hypotheses/Questions/Objectives------------------------------------
+
+# 2. Data Exploration------------------------------------------------------------
+
+# 3. Dependent Variable (DV) and Data Processing---------------------------------
+
+# 4. Independent Variables (IV) and Data Processing------------------------------
+
+# 5. Statistical Testing---------------------------------------------------------
+
+# 6. Reporting and Interpreting the Outcomes-------------------------------------
+
